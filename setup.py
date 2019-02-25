ee42ccc47859cadb54b66a40990030e8271072b9
Mindey <mindey@qq.com>
2019-02-25 20:39:01 +0000

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='metaculus',
-    version='0.0.1',
+    version='0.0.2',
     description='Metaculus controller.',
     url='https://gitlab.com/wefindx/metaculus',
     author='Mindey',
